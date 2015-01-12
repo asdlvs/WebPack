cf06650cdc9a9f69d67ea05161c645cc121c3c61
unknown <asdfghjklzxc@yandex.ru>
2015-01-12 09:40:33 +0300

    Add fake model

---
+++ Interaction.Web/Models/FakeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Interaction.Web.Models
+{
+    public class FakeModel : AngularModel
+    {
+        public FakeModel() : base("fake")
+        { }
+    }
+}
